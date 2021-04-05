fe7902c27b8ff511b6d97183e98bc0a7366e83cb
SAI <SAI@DESKTOP-GC2PO6S>
2021-04-05 23:42:29 +0530

    asdasdsd

--- src/test/java/com/amazon/qa/testcases/Sample.java
+++ src/test/java/com/amazon/qa/testcases/Sample.java
@@ -3,14 +3,10 @@ package com.amazon.qa.testcases;
 public class Sample {
 	
 	public void sum() {
-		
-<<<<<<< HEAD
-		
 		String a;
-=======
 		String bd;
->>>>>>> refs/heads/Saikiran
 	}
+
 
 	public static void main(String args[]) {}
 	
